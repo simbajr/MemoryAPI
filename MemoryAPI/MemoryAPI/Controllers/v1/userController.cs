d01aceee5fc0ecbc119bd5d8adf275e5a3154a0f
Hansrutger <bloody_pirate_4-ever@hotmail.com>
2016-02-07 14:43:41 +0100

    Calls

--- MemoryAPI/MemoryAPI/Controllers/v1/userController.cs
+++ MemoryAPI/MemoryAPI/Controllers/v1/userController.cs
@@ -9,31 +9,62 @@ namespace MemoryAPI.Controllers.v1
 {
     public class userController : ApiController
     {
-        // GET api/user
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/user/5
-        public string Get(int id)
+        // /api/v1/user/<username>/media-objects
+        // GET: Returns a list of media objects for the user. 
+        public string Get(String username)
         {
             return "value";
         }
 
-        // POST api/user
+        // /api/v1/user/<username>/media
+        // POST: Create new media file. In body must be either "sound-file", "video-file" or "picture-file".
         public void Post([FromBody]string value)
         {
+
         }
 
-        // PUT api/user/5
-        public void Put(int id, [FromBody]string value)
+        // /api/v1/user/<username>
+        // GET: Returns the user with username <username>.
+        public string Get(String username)
         {
+            return "value";
         }
 
-        // DELETE api/user/5
-        public void Delete(int id)
+        // DELETE: Deletes everything related to the user with username <username>.
+        public void Delete(String username)
         {
+
         }
+
+        // /api/v1/user/<username>/friends
+        // GET: Returns a list of friends the user with username <username> has.
+        public string Get(String username)
+        {
+            return "value";
+        }
+
+        // POST: Adds a user as a friend to the user with username <username>.
+        public void Post([FromBody]string value)
+        {
+
+        }
+
+        // /api/v1/user/<username>/media
+        // GET: Returns a list of media the user with username <username> has.
+        public string Get(String username)
+        {
+            return "value";
+        }
+
+
+        // /api/v1/user/<username>/friends/<friendsUsername>
+        // DELETE: Removes the user with username <friendsUsername> as a friend to the user with username <username>.
+        public void Delete(String username, String friendsUsername)
+        {
+
+        }
+
+
+
     }
 }
