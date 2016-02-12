925c12dd061ba79e78fd9130d4d2556ec46e4146
simbajr <simbajr@live.se>
2016-02-12 11:35:05 +0100

    Token and shit works

--- MemoryAPI/MemoryAPI/Startup.cs
+++ MemoryAPI/MemoryAPI/Startup.cs
@@ -18,8 +18,27 @@ namespace MemoryAPI
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+
+            ConfigurationAOuth(app);
+            
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+        public void ConfigurationAOuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+
+            };
+
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
         }
     }
 
